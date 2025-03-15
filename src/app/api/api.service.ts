a7dbab61a6c27ef3dcd7bfc85866f958aecd8d5c
gitfre4k <zatocilo@gmail.com>
2025-03-15 01:52:24 +0100

    temelj0

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,14 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, from } from 'rxjs';
+import { BehaviorSubject, from, map, Observable, of } from 'rxjs';
+
+interface Company {
+  id: string;
+  name: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
   private http: HttpClient = inject(HttpClient);
+  // companies: Company[];
 
   constructor() {}
+
+  getAccessibleTenants() {
+    return from(
+      this.http.get<Observable<Company[]>>(
+        'https://app.monitoringdriver.com/api/Tenant/GetAccessibleTenants',
+        { withCredentials: true }
+      )
+    );
+  }
+
+  // https://app.monitoringdriver.com/api/Violations/GetViolations
+
+  getViolations(tenantId: string) {
+    return this.http.post(
+      'https://app.monitoringdriver.com/api/Violations/GetViolations',
+      {
+        searchRule: { columns: ['driverName'], text: '' },
+        sorting: 'driverName asc',
+        skipCount: 0,
+        maxResultCount: 25,
+      },
+      {
+        withCredentials: true,
+        headers: {
+          'X-Tenant-Id': `${tenantId}`,
+        },
+      }
+    );
+  }
+
+  // '3a13ca94-18b5-41be-4a36-7f024111c52a'
 
   getDriverDailyLogEvents(
     driverId: number,
